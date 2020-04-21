f86def69d989b0b1255f12427217064f96fe71f1
mejgun <mejgun@gmail.com>
2020-04-21 18:29:36 +0300

    move C funcs to tdlib.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,28 +1,27 @@
-{-# LANGUAGE ForeignFunctionInterface #-}
+
 {-# LANGUAGE OverloadedStrings #-}
 
 
 module Main where
 
-import           Foreign
-import           Foreign.C.String
-import           Foreign.C.Types
+
 
 import           API.GeneralResult
 -- import           API.Ok
 import           API.Functions.SetLogVerbosityLevel
-import           Data.Aeson
+import           Data.Aeson                     ( decode
+                                                , encode
+                                                , ToJSON
+                                                )
 import qualified Data.ByteString               as B
 import qualified Data.ByteString.Lazy          as BL
+import           TDLib
 
-foreign import ccall "libtdjson td_json_client_create" c_create :: IO Client
-foreign import ccall "libtdjson td_json_client_send" c_send :: Client -> CString -> IO ()
-foreign import ccall "libtdjson td_json_client_receive" c_receive :: Client -> Timeout -> IO CString
-foreign import ccall "libtdjson td_json_client_execute" c_execute :: Client -> CString -> IO ()
-foreign import ccall "libtdjson td_json_client_destroy" c_destroy :: Client -> IO ()
 
-type Client = Ptr ()
-type Timeout = Double
+import           Foreign.C.String
+import           Foreign.C.Types
+import           Foreign
+
 
 -- main :: IO ()
 -- main = do
@@ -44,10 +43,9 @@ main = do
   --execute client testQuery2
   send
     client
-    (encode API.Functions.SetLogVerbosityLevel.SetLogVerbosityLevel
+    API.Functions.SetLogVerbosityLevel.SetLogVerbosityLevel
       { new_verbosity_level = 2
       }
-    )
   live client
   destroy client
  where
@@ -55,27 +53,11 @@ main = do
     cs <- receive c
     if cs /= nullPtr
       then do
-        is <- B.packCString cs
-        let j =
-              decode (BL.fromStrict is) :: Maybe API.GeneralResult.GeneralResult
-        print j
+        bs <- B.packCString cs
+        let s = BL.fromStrict bs
+        let z = decode s :: Maybe API.GeneralResult.GeneralResult
+        print z
       else return ()
     live c
 
-
-
-create :: IO Client
-create = c_create
-
-send :: Client -> BL.ByteString -> IO ()
-send c s = B.useAsCString (BL.toStrict s) (c_send c)
-
--- execute :: Client -> String -> IO ()
--- execute c s = (newCString s) >>= c_execute c
-
-receive :: Client -> IO CString
-receive c = c_receive c 1.0
-
-destroy :: Client -> IO ()
-destroy c = c_destroy c
 
