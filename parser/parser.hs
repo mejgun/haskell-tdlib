a92e7bcb3edfa551bf3bcb6f69983d131dc48f4d
mejgun <mejgun@gmail.com>
2020-04-30 15:45:04 +0300

    parser warnings fix

--- parser/parser.hs
+++ parser/parser.hs
@@ -28,21 +28,32 @@ main = do
   let f = renameWrongTypedField . parseParams . filterKeys $ functionsToMap b
 
   let g            = uniq $ makeGeneralResult (b)
-  writeToFiles api      api             (Map.insert "GeneralResult" g m) comments
+  writeToFiles api api (Map.insert "GeneralResult" g m) comments
   writeToFiles apifuncs "API.Functions" f comments
  where
   removeUnnecessary :: T.Text -> T.Text
   removeUnnecessary t =
-    T.unlines $ filter (\x -> let s = T.strip x in s /= T.empty && not (T.isPrefixOf "//" s)) $ T.lines t
+    T.unlines
+      $ filter
+          (\x -> let s = T.strip x in s /= T.empty && not (T.isPrefixOf "//" s)
+          )
+      $ T.lines t
 
-writeToFiles :: T.Text -> T.Text -> Map.Map T.Text [(T.Text, [(T.Text, T.Text)])] -> Map.Map T.Text [T.Text] -> IO ()
-writeToFiles addr modName m cmnts =
+writeToFiles
+  :: T.Text
+  -> T.Text
+  -> Map.Map T.Text [(T.Text, [(T.Text, T.Text)])]
+  -> Map.Map T.Text [T.Text]
+  -> IO ()
+writeToFiles addr modName mp cmnts =
   mapM_
       (\(a, b) -> do
-        writeFile (T.unpack (T.concat ["../src/", addr, "/", a, ".hs"]))      (wr a b)
-        writeFile (T.unpack (T.concat ["../src/", addr, "/", a, ".hs-boot"])) (whr a)
+        writeFile (T.unpack (T.concat ["../src/", addr, "/", a, ".hs"]))
+                  (wr a b)
+        writeFile (T.unpack (T.concat ["../src/", addr, "/", a, ".hs-boot"]))
+                  (whr a)
       )
-    $ Map.toList m
+    $ Map.toList mp
  where
   generated = "-- GENERATED\n"
   moduleName d = T.concat ["module ", modName, ".", d, " where\n\n"]
@@ -76,7 +87,7 @@ writeToFiles addr modName m cmnts =
     , "\ndata "
     , d
     , " = \n "
-    , T.intercalate " \n | " ww
+    , T.intercalate " \n | " wwgr
     , " deriving (Show, Eq)"
     , "\n\ndata ResultWithExtra = ResultWithExtra GeneralResult (Maybe String) deriving (Show, Eq)"
     , "\n\ninstance T.FromJSON ResultWithExtra where\n"
@@ -118,8 +129,8 @@ writeToFiles addr modName m cmnts =
       ]
     ]
    where
-    ww = map (\(a, b) -> T.concat [toTitle a, " ", (www b)]) e
-    www l = T.intercalate " " (map (\(_, b) -> b) l)
+    wwgr = map (\(a, b) -> T.concat [toTitle a, " ", (wwwgr b)]) e
+    wwwgr l = T.intercalate " " (map (\(_, b) -> b) l)
   -- write other .hs-boot files
   whb d = T.unpack $ T.concat
     [ generated
@@ -147,7 +158,8 @@ writeToFiles addr modName m cmnts =
     , "import qualified Data.Aeson.Types as T\n"
     , (imports $ keys e)
     , "\n"
-    , T.intercalate "\n-- \n" (Map.findWithDefault ["-- comments parse failed"] d cmnts)
+    , T.intercalate "\n-- \n"
+                    (Map.findWithDefault ["-- comments parse failed"] d cmnts)
     , "\ndata "
     , d
     , " = \n "
@@ -156,14 +168,25 @@ writeToFiles addr modName m cmnts =
     , toJsonString d e
     ]
   imports :: [T.Text] -> T.Text
-  imports ks = T.concat $ map (\k -> T.concat ["import {-# SOURCE #-} qualified ", api, ".", k, " as ", k, "\n"]) ks
+  imports ks = T.concat $ map
+    (\k -> T.concat
+      ["import {-# SOURCE #-} qualified ", api, ".", k, " as ", k, "\n"]
+    )
+    ks
   keys :: [(T.Text, [(T.Text, T.Text)])] -> [T.Text]
-  keys e = uniq $ foldl (\acc (_, b) -> (foldl (\acc2 (_, d) -> (myAdd (T.replace "Maybe " "" d) acc2)) acc b)) [] e
+  keys e = uniq $ foldl
+    (\acc (_, b) ->
+      (foldl (\acc2 (_, d) -> (myAdd (T.replace "Maybe " "" d) acc2)) acc b)
+    )
+    []
+    e
   ww :: [(T.Text, [(T.Text, T.Text)])] -> T.Text
-  ww e = T.intercalate " \n | " $ map (\(a, b) -> T.concat [toTitle a, www b]) e
-  www e = case T.intercalate ", " (map (\(a, b) -> T.concat [a, " :: ", b]) e) of
-    "" -> ""
-    p  -> T.concat [" { ", p, " } "]
+  ww e =
+    T.intercalate " \n | " $ map (\(a, b) -> T.concat [toTitle a, www b]) e
+  www e =
+    case T.intercalate ", " (map (\(a, b) -> T.concat [a, " :: ", b]) e) of
+      "" -> ""
+      p  -> T.concat [" { ", p, " } "]
   toJsonString :: T.Text -> [(T.Text, [(T.Text, T.Text)])] -> T.Text
   toJsonString m e = T.concat
     [ "\n\ninstance T.ToJSON "
@@ -174,13 +197,20 @@ writeToFiles addr modName m cmnts =
         [ "\n toJSON ("
         , toTitle a
         , " { "
-        , T.intercalate ", " $ filter (/= " = ") $ map (\(x, _) -> T.concat [x, " = ", x]) b
+        , T.intercalate ", " $ filter (/= " = ") $ map
+          (\(x, _) -> T.concat [x, " = ", x])
+          b
         , " }) =\n"
         , "  A.object [ "
         , T.intercalate ", "
         $ filter (/= "")
         $ T.concat ["\"@type\" A..= T.String \"", a, "\""]
-        : map (\(x, _) -> if x /= "" then T.concat ["\"", T.dropAround (== '_') x, "\" A..= ", x] else "") b
+        : map
+            (\(x, _) -> if x /= ""
+              then T.concat ["\"", T.dropAround (== '_') x, "\" A..= ", x]
+              else ""
+            )
+            b
         , " ]"
         ]
       )
@@ -193,7 +223,10 @@ writeToFiles addr modName m cmnts =
       [ "  t <- obj A..: \"@type\" :: T.Parser String\n"
       , "  case t of\n"
       , T.intercalate "\n"
-      $  (map (\(a, b) -> T.concat ["   \"", a, "\" -> parse", toTitle a, " v"]) e)
+      $  (map
+           (\(a, _) -> T.concat ["   \"", a, "\" -> parse", toTitle a, " v"])
+           e
+         )
       ++ ["   _ -> mempty"]
       , "\n  where\n"
       , T.intercalate "\n\n" $ map
@@ -211,7 +244,8 @@ writeToFiles addr modName m cmnts =
           , T.intercalate "\n"
           $  map
                (\(c, typ) -> if typ /= "Maybe Int"
-                 then T.concat ["    ", c, " <- o A..:? \"", T.dropAround (== '_') c, "\""]
+                 then T.concat
+                   ["    ", c, " <- o A..:? \"", T.dropAround (== '_') c, "\""]
                  else T.concat
                    [ "    "
                    , c
@@ -227,7 +261,8 @@ writeToFiles addr modName m cmnts =
                  [ "    return $ "
                  , toTitle a
                  , " { "
-                 , T.intercalate ", " $ map (\(c, _) -> T.concat [c, " = ", c]) b
+                 , T.intercalate ", "
+                   $ map (\(c, _) -> T.concat [c, " = ", c]) b
                  , " }"
                  ]
              ]
@@ -239,27 +274,44 @@ writeToFiles addr modName m cmnts =
   myAdd :: T.Text -> [T.Text] -> [T.Text]
   myAdd x xs = do
     let n@(n1 : n2) = T.split (== '.') x
-    if length (n) > 1 && T.dropAround (== '[') n1 == T.dropAround (== ']') (head n2)
-      then T.dropAround (== '[') (head n) : xs
-      else xs
+    if length (n) > 1 && T.dropAround (== '[') n1 == T.dropAround (== ']')
+                                                                  (head n2)
+    then
+      T.dropAround (== '[') (head n) : xs
+    else
+      xs
 
 -- | recursively rename (add _) record field name if there is same field with
 --   different type or if it is eq to haskell reserved word
-renameWrongTypedField :: Map.Map T.Text [(T.Text, [(T.Text, T.Text)])] -> Map.Map T.Text [(T.Text, [(T.Text, T.Text)])]
+renameWrongTypedField
+  :: Map.Map T.Text [(T.Text, [(T.Text, T.Text)])]
+  -> Map.Map T.Text [(T.Text, [(T.Text, T.Text)])]
 renameWrongTypedField = Map.map go
  where
   go :: [(T.Text, [(T.Text, T.Text)])] -> [(T.Text, [(T.Text, T.Text)])]
   go e =
-    let (_, l) = foldl (\(acc, ll) (x, y) -> let (m, tl) = gogo acc y in (m, (x, tl) : ll)) (Map.empty, []) e in l
-  gogo :: Map.Map T.Text T.Text -> [(T.Text, T.Text)] -> (Map.Map T.Text T.Text, [(T.Text, T.Text)])
-  gogo m e = foldl (\(acc, l) (a, b) -> let (mm, tl) = myInsert acc a b in (mm, tl : l)) (m, []) e
+    let (_, l) = foldl
+          (\(acc, ll) (x, y) -> let (m, tl) = gogo acc y in (m, (x, tl) : ll))
+          (Map.empty, [])
+          e
+    in  l
+  gogo
+    :: Map.Map T.Text T.Text
+    -> [(T.Text, T.Text)]
+    -> (Map.Map T.Text T.Text, [(T.Text, T.Text)])
+  gogo m e = foldl
+    (\(acc, l) (a, b) -> let (mm, tl) = myInsert acc a b in (mm, tl : l))
+    (m, [])
+    e
    where
     checkKey k v mm = case (== v) <$> Map.lookup k mm of
       Just False -> checkKey (addUndrscr k) v mm
       _          -> k
     myInsert mm a b = do
-      let key = if a `elem` ["type", "data", "id", "length", "error", "filter"] then addUndrscr a else a
-      let k   = checkKey key b mm
+      let key = if a `elem` ["type", "data", "id", "length", "error", "filter"]
+            then addUndrscr a
+            else a
+      let k = checkKey key b mm
       (Map.insert k b mm, (k, b))
 
 addUndrscr :: T.Text -> T.Text
@@ -271,7 +323,9 @@ addUndrscr a = T.concat ["_", a]
 --   to map["ChatPhoto"]=[("chatPhoto", [("small","file"), ("big","file")])]
 --
 --   map[(data type name)]=[(data type contructor), [((record field name),(record field type))]
-parseParams :: Map.Map T.Text [(T.Text, T.Text)] -> Map.Map T.Text [(T.Text, [(T.Text, T.Text)])]
+parseParams
+  :: Map.Map T.Text [(T.Text, T.Text)]
+  -> Map.Map T.Text [(T.Text, [(T.Text, T.Text)])]
 parseParams = Map.mapWithKey (\k v -> map (\(a, b) -> (a, go k b)) v)
  where
   -- go: first argument - current module name.
@@ -293,23 +347,37 @@ parseParams = Map.mapWithKey (\k v -> map (\(a, b) -> (a, go k b)) v)
     "double" -> "Float"
     p ->
       let t = toTitle $ checkList aMod p
-      in  if t == aMod then aMod else if T.isInfixOf "[" t then t else T.concat [t, ".", t]
+      in  if t == aMod
+            then aMod
+            else if T.isInfixOf "[" t then t else T.concat [t, ".", t]
   checkList aMod s = do
     let s1 = T.replace "vector<" "" $ T.replace ">" "" s
     if s1 == s then s1 else T.concat ["[", conv aMod s1, "]"]
 
 
 -- | remove default data types from top level
-filterKeys :: Map.Map T.Text [(T.Text, T.Text)] -> Map.Map T.Text [(T.Text, T.Text)]
-filterKeys m = Map.withoutKeys m
-  $ Set.fromList ["Double", "String", "Int32", "Int53", "Int64", "Bytes", "Bool", "Bool", "Vector t", ""]
+filterKeys
+  :: Map.Map T.Text [(T.Text, T.Text)] -> Map.Map T.Text [(T.Text, T.Text)]
+filterKeys m = Map.withoutKeys m $ Set.fromList
+  [ "Double"
+  , "String"
+  , "Int32"
+  , "Int53"
+  , "Int64"
+  , "Bytes"
+  , "Bool"
+  , "Bool"
+  , "Vector t"
+  , ""
+  ]
 
 -- | convert "chatPhoto small:file big:file = ChatPhoto;" to
 -- map["ChatPhoto"]=[("chatPhoto", "small:file big:file")]
 --
 -- map(data type name)=[(data type contructor), (string)]
 toMap :: [T.Text] -> Map.Map T.Text [(T.Text, T.Text)]
-toMap t = foldl (\acc (k, v) -> Map.insertWith (++) k [v] acc) Map.empty $ map format t
+toMap t = foldl (\acc (k, v) -> Map.insertWith (++) k [v] acc) Map.empty
+  $ map format t
  where
   format l = do
     let (a, name) = T.breakOn "=" l
@@ -319,10 +387,11 @@ toMap t = foldl (\acc (k, v) -> Map.insertWith (++) k [v] acc) Map.empty $ map f
 -- convert "setPassportElement element:InputPassportElement password:string = PassportElement;"
 -- to map[setPassportElement]=[("setPassportElement", "element:InputPassportElement password:string")]
 functionsToMap :: [T.Text] -> Map.Map T.Text [(T.Text, T.Text)]
-functionsToMap t = foldl (\acc (k, v) -> Map.insertWith (++) k [v] acc) Map.empty $ map format t
+functionsToMap t =
+  foldl (\acc (k, v) -> Map.insertWith (++) k [v] acc) Map.empty $ map format t
  where
   format l = do
-    let (a, result)  = T.breakOn "=" l
+    let (a, _)       = T.breakOn "=" l
     let (name, rest) = T.breakOn " " a
     (toTitle (myStrip name), (myStrip name, myStrip rest))
 
@@ -338,16 +407,25 @@ uniq l = foldl (\acc x -> if (x `elem` acc) then acc else x : acc) [] l
 
 makeGeneralResult :: [T.Text] -> [(T.Text, [(T.Text, T.Text)])]
 makeGeneralResult l = foldl
-  (\acc x -> if length (x) > 1 then let n = myStrip (last x) in (n, [("", T.concat [n, ".", n])]) : acc else acc)
+  (\acc x -> if length (x) > 1
+    then let n = myStrip (last x) in (n, [("", T.concat [n, ".", n])]) : acc
+    else acc
+  )
   []
   (map (T.splitOn "=") l)
 
 
 parseComments :: T.Text -> Map.Map T.Text [T.Text]
 parseComments l = do
-  let s  = tail $ T.splitOn "\n//@description " l
-  let s1 = map (\y -> filter (\x -> (x /= T.empty) && not (T.isPrefixOf (T.pack "@class ") x)) $ T.splitOn "\n//" y) s
-  let m  = Map.empty :: Map.Map T.Text [T.Text]
+  let s = tail $ T.splitOn "\n//@description " l
+  let
+    s1 = map
+      (\y ->
+        filter (\x -> (x /= T.empty) && not (T.isPrefixOf (T.pack "@class ") x))
+          $ T.splitOn "\n//" y
+      )
+      s
+  let m = Map.empty :: Map.Map T.Text [T.Text]
   let m1 = foldl
         (\acc t ->
           let x = T.splitOn "\n" $ last t
@@ -367,7 +445,10 @@ parseComments l = do
         m1
         c1
   --Map.map (\t -> "-- |" : map (\t -> T.concat ["-- ", t]) t) m2
-  let m3 = Map.map (\t -> foldr (\x acc -> (filter (/= T.empty) (splitAtAt x)) ++ acc) [] t) m2
+  let m3 = Map.map
+        (\t -> foldr (\x acc -> (filter (/= T.empty) (splitAtAt x)) ++ acc) [] t
+        )
+        m2
   Map.map (\t -> "-- |" : map (\x -> T.concat ["-- ", x]) t) m3
  where
   toHyperlinkFirst :: T.Text -> T.Text
@@ -376,6 +457,8 @@ parseComments l = do
         x1       = T.concat ["__", x, "__"]
     in  T.unwords (x1 : xs)
   splitAtAt :: T.Text -> [T.Text]
-  splitAtAt t = let (x : xs) = T.splitOn "@" t in if length xs > 0 then x : map toHyperlinkFirst xs else [t]
+  splitAtAt t =
+    let (x : xs) = T.splitOn "@" t
+    in  if length xs > 0 then x : map toHyperlinkFirst xs else [t]
 
 
