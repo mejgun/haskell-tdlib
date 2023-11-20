a7ba0223421e92025813869053f2acaa8864752d
mejgun <mejgun@gmail.com>
2023-11-20 11:24:57 +0300

    update (TDlib 1.8.21)

--- src/TD/Data/WebPage.hs
+++ src/TD/Data/WebPage.hs
@@ -31,7 +31,7 @@ data WebPage
     , duration             :: Maybe Int                         -- ^ Duration of the content, in seconds
     , author               :: Maybe T.Text                      -- ^ Author of the content
     , has_large_media      :: Maybe Bool                        -- ^ True, if size of media in the preview can be changed
-    , show_large_media     :: Maybe Bool                        -- ^ True, if large media preview must be shown
+    , show_large_media     :: Maybe Bool                        -- ^ True, if large media preview must be shown; otherwise, the media preview must be shown small and only the first frame must be shown for videos
     , skip_confirmation    :: Maybe Bool                        -- ^ True, if there is no need to show an ordinary open URL confirmation, when opening the URL from the preview, because the URL is shown in the message text in clear
     , show_above_text      :: Maybe Bool                        -- ^ True, if the link preview must be shown above message text; otherwise, the link preview must be shown below the message text
     , animation            :: Maybe Animation.Animation         -- ^ Preview of the content as an animation, if available; may be null
