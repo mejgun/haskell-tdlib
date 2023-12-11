16c42fa97f565be64448d128a8442c34c1dafb2f
mejgun <mejgun@gmail.com>
2023-12-11 11:01:19 +0300

    update (TDlib 1.8.22)

--- src/TD/Data/PremiumGiveawayParameters.hs
+++ src/TD/Data/PremiumGiveawayParameters.hs
@@ -13,7 +13,7 @@ data PremiumGiveawayParameters
     { boosted_chat_id        :: Maybe Int      -- ^ Identifier of the channel chat, which will be automatically boosted by the winners of the giveaway for duration of the Premium subscription
     , additional_chat_ids    :: Maybe [Int]    -- ^ Identifiers of other channel chats that must be subscribed by the users to be eligible for the giveaway. There can be up to getOption("giveaway_additional_chat_count_max") additional chats
     , winners_selection_date :: Maybe Int      -- ^ Point in time (Unix timestamp) when the giveaway is expected to be performed; must be 60-getOption("giveaway_duration_max") seconds in the future in scheduled giveaways
-    , only_new_members       :: Maybe Bool     -- ^ True, if only new subscribers of the chats will be eligible for the giveaway
+    , only_new_members       :: Maybe Bool     -- ^ True, if only new members of the chats will be eligible for the giveaway
     , country_codes          :: Maybe [T.Text] -- ^ The list of two-letter ISO 3166-1 alpha-2 codes of countries, users from which will be eligible for the giveaway. If empty, then all users can participate in the giveaway. There can be up to getOption("giveaway_country_count_max") chosen countries. Users with phone number that was bought on Fragment can participate in any giveaway and the country code "FT" must not be specified in the list
     }
   deriving (Eq, Show)
